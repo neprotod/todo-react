d43d31860c0da1a7d61eb155020a605afd934187
Alexander Podlesnyi <gwebdzen@gmail.com>
2019-12-15 23:26:02 +0200

    Updates

---
+++ precache-manifest.7032e5f23fa2a177c4bd8e94cd718358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3362e30b17d1dd72e26907033989134a",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "fdaec5832790268c1efd",
+    "url": "/todos/static/css/main.e8c012c5.chunk.css"
+  },
+  {
+    "revision": "c8c32fc6a7f91f77070a",
+    "url": "/todos/static/js/2.7cb7ff73.chunk.js"
+  },
+  {
+    "revision": "fdaec5832790268c1efd",
+    "url": "/todos/static/js/main.9b86b760.chunk.js"
+  },
+  {
+    "revision": "58f72a9928444ac95a71",
+    "url": "/todos/static/js/runtime-main.7fb49737.js"
+  }
+]);
