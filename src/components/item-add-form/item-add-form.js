61fb0ab14fe4002c3b35f974a7570e0ed487f494
Alexander Podlesnyi <gwebdzen@gmail.com>
2019-12-15 21:25:52 +0200

    Add gh-pages package.json

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,14 +3,22 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component{
+    state = {
+        input: ''
+    }
+
     submitForm = (e) => {
         e.preventDefault();
-        const input = e.target.querySelector('input');
-        const value = input.value;
-        input.value = '';
-
-        this.props.onAdd(value);
+        this.props.onAdd(this.state.input);
+        this.setState({input:''});
     }
+
+    handlerInput = (e) => {
+        this.setState({
+            input: e.target.value
+        });
+    }
+
     render(){
         // const {onAdd} = this.props;
         return (
@@ -18,7 +26,9 @@ export default class ItemAddForm extends Component{
                 <input 
                     className="form-control" 
                     type="text"
-                    placeholder="What needs to be done"/>
+                    placeholder="What needs to be done"
+                    value={this.state.input}
+                    onChange={this.handlerInput}/>
                 <button className="btn btn-outline-secondary">Add item</button>
             </form>
         )
